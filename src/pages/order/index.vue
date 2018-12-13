5ddd65719832fcd9defde8fd976ceeafb13c84c7
chenxili <1421025233@qq.com>
2018-12-13 13:58:36 +0800

    save

--- src/pages/order/index.vue
+++ src/pages/order/index.vue
@@ -1,6 +1,27 @@
 <template>
     <div class="order-container">
-    
+        <div class="header">
+            <ul>
+                <li>
+                    全部
+                </li>
+                <li>
+                    待支付
+                </li>
+                <li>
+                    待发货
+                </li>
+                <li>
+                    已完成
+                </li>
+            </ul>
+        </div>
+        <div class="order-list">
+        
+        </div>
+        <div class="no-result">
+
+        </div>
     </div>
 </template>
 <script>
@@ -8,4 +29,13 @@ export default {
 }
 </script>
 <style lang="less">
+.order-container {
+    .header {
+        ul {
+            li {
+                
+            }
+        }
+    }
+}
 </style>
